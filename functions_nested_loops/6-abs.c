a32eb5e99e4daaf9116d593e196ce926d20006c8
Scandere78 <scanderetejpro@gmail.com>
2024-02-23 10:57:48 +0100

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Écrire une fonction qui calcule la valeur absolue d’un entier.
+ *@n: function parameter to be checked
+ * Return: -n or n;
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else if (n >= 0)
+	{
+		return (n);
+	}
+}
