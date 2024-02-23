f4c72c32b2d8f300506cac1d7cc5fb34f525349d
Scandere78 <scanderetejpro@gmail.com>
2024-02-23 12:06:43 +0100

    print last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ *@i: function parameter to be checked
+ * Return: lastd;
+ */
+
+int print_last_digit(int i)
+{
+	int lastd;
+
+	lastd = i % 10;
+	if (i < 0)
+		lastd = -lastd;
+	_putchar(lastd + '0')
+	return (lastd);
+}
