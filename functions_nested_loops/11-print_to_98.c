fe413448fe136b838fd64817639ce2512ea50d5a
Scandere78 <scanderetejpro@gmail.com>
2024-02-24 19:33:53 +0100

    +

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,12 +3,45 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 void print_to_98(int n)
 {
+    int i;
+    if (n<99){
+        for(i=n; i < 99; i++)
+        {
+            if (i>99) {
+                _putchar((i/100)+'0');
+                _putchar((i/10)+'0');
+                _putchar((i%10)+'0');
+            } else if (i>9) {
+                _putchar((i/10)+'0');
+                _putchar((i%10)+'0');
+            } else {
+                _putchar((i%10)+'0');
+            }
+            if(i != 98) {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    } else {
+        for(i=n; i > 99; i--)
+        {
+            if (i<10) {
+                _putchar((i%10)+'0');
+            } else {
+                _putchar((i/10)+'0');
+                _putchar((i%10)+'0');
+            }
+            if(i != 98) {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
 
-	for (n = 0 ; n < 99)
-
-	_putchar(',');
-	_putchar(' ');
+    }
+}
