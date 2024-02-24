c778d6a7ab95bad0b309e1cee06cd53e45b57bfe
Scandere78 <scanderetejpro@gmail.com>
2024-02-24 16:41:58 +0100

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include <stdio.h> 
+#include "main.h"
+
+/**
+  * print_times_table - Prints a multiplication table up to param
+  * @n: The number to be treated
+  *
+  * Return: Number matrix
+  */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (a = 0 ; a < 10 ; a++)
+		{
+			b = a * c;
+			
+			if ( b < 10) {
+				_putchar((b % 10) + '0');
+			
+			} else {
+				_putchar((b / 10) + '0');
+				_putchar((b % 10) + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	
+}
