302f30e1ae8815ba924e10135e5fb1940c1b4130
Scandere78 <scanderetejpro@gmail.com>
2024-02-28 10:50:30 +0100

    technique de permutation

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * swap_int - Update pointer
+ * @a: (int) A swap to B
+ * @b: (int) B swap to A
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
