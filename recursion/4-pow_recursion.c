c421235803242d447e490e94be630d53ba6026b2
Scandere78 <scanderetejpro@gmail.com>
2024-03-15 16:08:00 +0100

    cette fonction utilise la récursion pour calculer la puissance d'un nombre en multipliant le nombre de base par lui-même, répétant cette opération jusqu'à ce que l'exposant soit réduit à zéro.

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: int
- * @y: int
+ * _pow_recursion - x raised to the power of y
+ * @x: number 1
+ * @y: number 2
+ * Return: (int) result
  */
-
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
@@ -16,6 +16,5 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-
-	return (x * _pow_recursion(x, (y - 1)));
+	return (x * _pow_recursion(x, y - 1));
 }
