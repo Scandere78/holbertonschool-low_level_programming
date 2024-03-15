e042cf2938a9be7b3b04f119100e9a46691671dc
Scandere78 <scanderetejpro@gmail.com>
2024-03-15 06:03:39 +0100

    supprimer les lettre au debut

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,43 +1,25 @@
 #include "main.h"
-#include <stdio.h>
+#include <stddef.h>
 
 /**
- * _strchr - locates a character in a string
- * @s: string
- * @c: character
- * Return: s or NULL
+ * _strchr - Locate a character in a string
+ * @s: String
+ * @c: Character to search
+ * Return: Pointer first occurence of @c in @s, or NULL
  */
-
 char *_strchr(char *s, char c)
 {
-	int length = _strlen(s);
-	int i;
-
-	for (i = 0; i < length; i++)
-	{
-		s++;
-		if (s[i] == c)
-		{		return (s);
+	while (*s)
+		{
+		if (*s == c)
+		{
+			return (s);
 		}
+		s++;
 	}
-return (NULL);
-}
-
-/**
- * _strlen - return the length of a string
- * @s: length of a string
- * Return: void
- */
-
-int _strlen(char *s)
-
-{
-	int i;
-	int length = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (*s == c)
 	{
-		length++;
+		return (s);
 	}
-	return (length);
+	return (NULL);
 }
