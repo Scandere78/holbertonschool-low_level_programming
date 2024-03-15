de61b0d68c1c206f0f8e38909196c261596183bc
Scandere78 <scanderetejpro@gmail.com>
2024-03-15 17:36:46 +0100

    cette fonction utilise la récursion pour calculer la racine carrée d'un nombre en vérifiant chaque carré d'index jusqu'à ce que la racine carrée soit trouvée ou que l'index dépasse la valeur de n.

--- recursion/main.h
+++ recursion/main.h
@@ -1,11 +1,28 @@
 int _putchar(char c);
+
+void _puts_recursion(char *s);
+
 void _print_rev_recursion(char *s);
+
+void __rev_recursion(char *s, int direction);
+
 int _strlen_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
+
+int __strlen_count(char *s);
+
 int factorial(int n);
+
 int _pow_recursion(int x, int y);
+
+int is_prime_number(int n);
+int check_prime_number(int i, int n);
+
 int _sqrt_recursion(int n);
 int __sqrt(int n, int i);
-int is_prime_number(int n)i;
-int check_prime_number(int i, int n);
+
+int is_palindrome(char *s);
+int __is_palindrome(char *s, int size, int *a, int *b);
+
+int wildcmp(char *s1, char *s2);
+int __wildcmp(char *s1, char *s2, int size1, int size2, int p1, int p2);
+int __wildcmp_check_ast(char *s1, char *s2, int size1, int size2, int p1, int p2);
