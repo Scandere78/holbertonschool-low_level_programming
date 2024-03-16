004aef45b4a5aea5911c87b79a66d7e540d54303
Scandere78 <scanderetejpro@gmail.com>
2024-03-16 16:41:52 +0100

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,19 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
 
-#include <stddef.h>
-
+/**
+ * struct dog - Dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: Structure of a dog
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
@@ -16,6 +22,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _strlen(char *str);
-char *_strcpy(char *dest, char *src);
 #endif
