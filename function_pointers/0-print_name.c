b7f24ac9fd96f5ec256eef9ed3fc33049d1187f4
Scandere78 <scanderetejpro@gmail.com>
2024-03-18 14:55:45 +0100

    print_name est conçue pour prendre en entrée un nom (sous forme de chaîne de caractères) ainsi qu'un pointeur de fonction

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Print name of Bob 
+ * @name: name of the person
+ * @f: pointeur 
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
