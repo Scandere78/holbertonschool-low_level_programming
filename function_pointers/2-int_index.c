feb9f5f7188de5ca8eabbc47243a0d9edee9f8ee
Scandere78 <scanderetejpro@gmail.com>
2024-03-20 11:38:11 +0100

    2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,23 +1,22 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * int_index - Search for an integer
- * @array: Array of element
- * @size: Size of array
- * @cmp: Function
+ * int_index - function
+ * @array: int ptr
+ * @size: int
+ * @cmp: int function ptr
+ *
+ * Return: int
  */
-int int_index(int *array, int size, int (*cmp)(int))
+int	int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int	i;
 
-	if (size > 0)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if ((*cmp)(array[i]) != 0) {
-				return (i);
-			}
-		}
-	}
+	if (array == 0 || size < 1 || cmp == 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
 	return (-1);
 }
