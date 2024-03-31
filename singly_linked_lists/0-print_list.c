ffae0d404fb9e3fb2ffef967f4a08cb7ed2d5216
Scandere78 <scanderetejpro@gmail.com>
2024-03-31 17:07:56 +0200

    add files 1 à 4

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,17 +3,17 @@
  * print_list - print all the elements of a list_t list
  * @h: singly linked list
  * Return: The number of nodes
- * */
+ */
 
 size_t print_list(const list_t *h)
 {
 	size_t nodes = O;
 
-	while (h) 
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
