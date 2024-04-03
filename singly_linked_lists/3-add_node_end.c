14125a7dc83e7770e606fb098f2ae638d132a8e5
Scandere78 <scanderetejpro@gmail.com>
2024-04-03 14:51:55 +0200

    add files

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,63 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * add_node_end - function that adds a new node
- * at the end of a list_t list.
- *
- * @head: Parameter that point to de head of the
- * linked list
- * @str: Parameter with the string
- *
- * Return: the address of the new element,
- * or NULL if it failed
+ * add_node_end - Add a new node at the end of list_t
+ * @head: Head
+ * @str: String
+ * Return: Adress of new element or NULL
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *temp;
+	list_t *new;
+	list_t *temp;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
-	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = _strlen(new->str);
 	new->next = NULL;
 
-	temp = *head;
 	if (*head == NULL)
 	{
 		*head = new;
 	}
 	else
 	{
-		while (temp->next)
+		temp = *head;
+		while (temp->next != NULL)
 		{
 			temp = temp->next;
 		}
 		temp->next = new;
 	}
-
 	return (new);
+}
+
+/**
+ * _strlen - Return length of a string
+ * @s: (int) text
+ * Return: (int) length
+ */
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
 }
