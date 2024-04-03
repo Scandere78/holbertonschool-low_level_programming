be12186224e288132efa2d362dc9e760bb075a3f
Scandere78 <scanderetejpro@gmail.com>
2024-04-03 14:41:06 +0200

    add files

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,49 +1,28 @@
 #include "lists.h"
-#include <string.h>
-#include <stdlib.h>
-
 /**
- * add_node - Add a new node at the beginning of list_t
- * @head: Head
- * @str: String
- * Return: list_t
+ * add_node - function that adds a new node
+ * at the beginning of a list_t list.
+ *
+ * @head: Parameter with the head node
+ * @str: parameter with the element str in
+ * the structure
+ *
+ * Return: Return the head of the linked list
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new;
 
-	if (head == NULL || str == NULL)
-		return (NULL);
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
-	new_node->len = _strlen(new_node->str);
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
-}
 
-/**
- * _strlen - Return length of a string
- * @s: (int) text
- * Return: (int) length
- */
-int _strlen(char *s)
-{
-	int i;
-	int length = 0;
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		length++;
-	}
-	return (length);
+	return (*head);
 }
