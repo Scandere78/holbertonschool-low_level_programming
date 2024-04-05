763592ff57006e11fbe56906032795195dfe86d8
Scandere78 <scanderetejpro@gmail.com>
2024-04-06 00:29:45 +0200

    test

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,13 +1,18 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
+ * print_binary - function  that returns the value of a bit
+ * @n: unsigned int
+ * Return: void
  */
+
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
 
-	_putchar((n & 1) + '0');
+	if (n & 1)
+		putchar('1');
+	else
+		putchar('0');
 }
