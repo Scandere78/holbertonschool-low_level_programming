61e43d68669b963bc581b2728f4682ae383a804a
Scandere78 <scanderetejpro@gmail.com>
2024-04-06 00:36:37 +0200

    test

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,20 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * get_bit - Gets the value of a bit at a given index.
- * @n: The bit.
- * @index: The index to get the value at - indices start at 0.
- *
- * Return: If an error occurs - -1.
- *         Otherwise - The value of bit at index.
+ * get_bit - get a bit
+ * @n: un nombr(il)
+ * @index: the index
+ * Return: the result
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	unsigned long int result = n >> index;
+
+	if (index >= sizeof(unsigned long int) * CHAR_BIT)
 		return (-1);
-
-	if ((n & (1 << index)) == 0)
-		return (0);
-
-	return (1);
+	return (result & 1);
 }
