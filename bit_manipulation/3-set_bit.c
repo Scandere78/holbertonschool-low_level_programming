647be602f43315db26e0b897f7aed729d6f74d40
Scandere78 <scanderetejpro@gmail.com>
2024-04-06 00:41:34 +0200

    test

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,19 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * set_bit - Sets the value of a bit at a given index to 1.
- * @n: A pointer to the bit.
- * @index: The index to set the value at - indices start at 0.
- *
- * Return: If an error occurs - -1.
- *         Otherwise - 1.
+ * set_bit - set a bit
+ * @n: une ombre au masculin
+ * @index: index, c'est parlant non?
+ * Return: 1 or -1
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	unsigned long int set = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * CHAR_BIT)
 		return (-1);
 
-	*n ^= (1 << index);
-
+	*n |= set;
 	return (1);
 }
